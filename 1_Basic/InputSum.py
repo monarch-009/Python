eba00d2fb19eed4b1811d827b65d1ccaf2dd40af
Aditya <adityasinghmoni@gmail.com>
2025-07-01 21:27:32 +0530

    Input

---
+++ 1_Basic/InputSum.py
@@ -0,0 +1,4 @@
+a = int(input("Enter an integer: "))
+b = int(input("Enter another integer: "))
+
+print("The sum of the two integers is:", a + b)
---
+++ 1_Basic/input.py
@@ -0,0 +1,9 @@
+# Take input from the user and print it
+
+a = input("Enter a number: ")
+b = input("Enter a string: ")
+
+print("You entered number:", a)
+print("You entered string:", b)
+
+
